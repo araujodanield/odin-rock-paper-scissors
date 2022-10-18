5ee7be6e5f858f6e6c57427b2793db598ab8acaa
araujodanield <daniel-araujo2021@outlook.com>
2022-10-17 23:50:27 -0300

    Fix the functions and makes possible play a single round

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 let choices = ["Rock", "Paper", "Scissors"];
 
-//computer random choice
+//Computer random choice
 function getComputerChoice () {
     let random = Math.floor(Math.random() * choices.length);
 
     return choices[random];
 }
 
-//compare the user input with the choices array
+let resetCompare; //reset for the `comparePlayerChoice` function in case of invalid input
+
+//Compare the user input with the `choices` array
 function comparePlayerChoice () {
     let userChoice = prompt("Choose your weapon:");
-
+    
     if (userChoice.toLowerCase() === choices[0].toLowerCase() || userChoice.toLowerCase() === choices[1].toLowerCase() || userChoice.toLowerCase() === choices[2].toLowerCase()) {
         return userChoice[0].toUpperCase() + userChoice.slice(1).toLowerCase();
     } else {
-        alert ('Invalid value! The options are: "Rock", "Paper" or "Scissors"!');
+        alert ('Invalid value! Please write: "Rock", "Paper" or "Scissors"!');
+        resetCompare = comparePlayerChoice();
+        return resetCompare;
     }
 }
 
@@ -23,26 +27,23 @@ function playRound (playerSelection, computerSelection) {
     playerSelection = comparePlayerChoice();
     computerSelection = getComputerChoice();
 
-    let result = "You choose " + playerSelection + ", and computer choose " + computerSelection;
+    let result = "You choose " + playerSelection + ", Computer choose " + computerSelection;
 
     console.log (result);
     
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-        alert ("EMPATE!");
+        return "It's a DRAW!";
     }  else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
-        alert ("VOCÊ GANHOU! PEDRA GANHA DE TESOURA!");
+        return "You WON this round! Rock beats Scissors!";
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
-        alert ("VOCÊ GANHOU! PAPEU GANHA DE PEDRA!");
+        return "You WON this round! Paper beats Rock!";
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
-        alert ("VOCÊ GANHOU! TESOURA GANHA DE PAPEL!");
+        return "You WON this round! Scissors beats Paper!";
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
-        alert ("VOCÊ PERDEU! PAPEU GANHA DE PEDRA!");
+        return "You LOSE this round! Paper beats Rock!";
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
-        alert ("VOCÊ PERDEU! TESOURA GANHA DE PAPEL!");
+        return "You LOSE this round! Scissors beats Paper!";
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
-        alert ("VOCÊ PERDEU! PEDRA GANHA DE TESOURA!");
+        return "You LOSE this round! Rock beats Scissors!";
     }
 }
-
-//THINGS TO DO:
-//DESCOBRIR COMO FAZER O PROMPT RESETAR CASO O VALOR SEJA INVALIDO
