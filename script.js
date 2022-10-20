26f9cc188351b9e45af95b0824c7566be960be8b
araujodanield <daniel-araujo2021@outlook.com>
2022-10-20 00:53:46 -0300

    Fix the cancel button and add the five rounds function

--- script.js
+++ script.js
@@ -1,49 +1,96 @@
 let choices = ["Rock", "Paper", "Scissors"];
+let userChoice;
+let playerScore;
+let computerScore;
+let cancelMessage = "You clicked Cancel. Please, start again.";
 
-//Computer random choice
-function getComputerChoice () {
+
+//Computer random choice;
+function getComputerChoice() {
     let random = Math.floor(Math.random() * choices.length);
 
     return choices[random];
-}
+};
 
-let resetCompare; //reset for the `comparePlayerChoice` function in case of invalid input
-
-//Compare the user input with the `choices` array
-function comparePlayerChoice () {
-    let userChoice = prompt("Choose your weapon:");
+//Compare the user input with the 'choices' array;
+function compareUserChoice() {
+    userChoice = prompt("Choose your weapon:");
     
-    if (userChoice.toLowerCase() === choices[0].toLowerCase() || userChoice.toLowerCase() === choices[1].toLowerCase() || userChoice.toLowerCase() === choices[2].toLowerCase()) {
+    if (userChoice === null) {
+        return cancelMessage;
+    } else if (userChoice.toLowerCase() === choices[0].toLowerCase() || userChoice.toLowerCase() === choices[1].toLowerCase() || userChoice.toLowerCase() === choices[2].toLowerCase()) {
         return userChoice[0].toUpperCase() + userChoice.slice(1).toLowerCase();
     } else {
-        alert ('Invalid value! Please write: "Rock", "Paper" or "Scissors"!');
-        resetCompare = comparePlayerChoice();
-        return resetCompare;
-    }
-}
+        alert ("OOPS, I didn't understand what you said. Please choose again between Rock, Paper, or Scissors!");
+        return compareUserChoice();
+    };
+};
 
-//play a single round
+
+//Play a single round;
 function playRound (playerSelection, computerSelection) {
-    playerSelection = comparePlayerChoice();
+    playerSelection = compareUserChoice();
     computerSelection = getComputerChoice();
 
-    let result = "You choose " + playerSelection + ", Computer choose " + computerSelection;
-
-    console.log (result);
+    let result = "You choose: " + playerSelection + " | Computer choose: " + computerSelection;
     
-    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-        return "It's a DRAW!";
+    if (userChoice === null) {
+        return cancelMessage;
+    } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        console.log (result);
+        console.log ("It's a DRAW!");
     }  else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
-        return "You WON this round! Rock beats Scissors!";
+        console.log (result);
+        console.log ("You WON this round! Rock beats Scissors!");
+        playerScore++;
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
-        return "You WON this round! Paper beats Rock!";
+        console.log (result);
+        console.log ("You WON this round! Paper beats Rock!");
+        playerScore++;
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
-        return "You WON this round! Scissors beats Paper!";
+        console.log (result);
+        console.log ("You WON this round! Scissors beats Paper!");
+        playerScore++;
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
-        return "You LOSE this round! Paper beats Rock!";
+        console.log (result);
+        console.log ("You LOSE this round! Paper beats Rock!");
+        computerScore++;
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
-        return "You LOSE this round! Scissors beats Paper!";
+        console.log (result);
+        console.log ("You LOSE this round! Scissors beats Paper!");
+        computerScore++;
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
-        return "You LOSE this round! Rock beats Scissors!";
+        console.log (result);
+        console.log ("You LOSE this round! Rock beats Scissors!");
+        computerScore++;
+    };
+};
+
+//Five rounds game;
+function game() {
+    playerScore = 0;
+    computerScore = 0;
+
+    alert ("WELCOME! You'll play 5 rounds of 'Rock Paper Scissors' against the machine, with the fate of the world in line, and your weapons are, well... I think you already know. GIVE YOUR BEST and GOOD LUCK!");
+
+    for (let i = 0; i < 5; i++) {
+        playRound();
+
+        if (userChoice === null) {
+            alert ("Are you running off the battle? It's ok, I'll wait until you are ready to try again. SEE YA!")
+            return cancelMessage;
+        } else if (i < 4) {
+            console.log ("You: " + playerScore + " | Computer: " + computerScore);
+            console.log (" ");
+        } else if (i = 4) {
+            console.log ("You: " + playerScore + " | Computer: " + computerScore);
+            if (playerScore === computerScore) {
+                return "WOW! THE GAME ENDED IN A DRAW! DOES THAT MEAN HUMANS AND MACHINES THINK THE SAME? hmm";
+            } else if (playerScore > computerScore) {
+                return "CONGRATULATIONS!!! YOU WON THE BATTLE AND GRANTED OUR WORLD A TOMORROW! :D";
+            } else {
+                return "OH NO! YOU LOST THE BATTLE AND NOW WE'LL BECOME TOYS FOR THE MACHINES! D:";
+            }
+        }
     }
 }
